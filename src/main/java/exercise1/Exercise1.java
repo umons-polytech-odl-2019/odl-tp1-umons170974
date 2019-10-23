3de02186cec7b7b868e114b127c42bc80e1e9902
Manuela <170974@umons.ac.be>
2019-10-23 15:00:48 +0200

    :)

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -1,16 +1,22 @@
 package exercise1;
 
+import examples.ClassWithAccessors;
+
 public class Exercise1 {
 	static Person createPerson(String name, int age) {
-		// Ajoutez les champs name et age à la classe Person.
+		// Ajoutez les champs name et age à la classe Person
 		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
 		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
-		return null;
+		Person p = new Person(name, age);
+		return p;
 	}
 
 	public static void main(String[] args) {
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
+		Person p=new Person(args[0], Integer.parseInt(args[1]));
+		System.out.println(p.getName()+ " " +p.getAge());
+
 	}
 }
--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,32 @@
 package exercise1;
 
 class Person {
+	private String name;
+	private int age;
+
+	public Person (String monnom, int monage)
+	{
+		name=monnom;
+		age=monage;
+	}
+
+	public String getName ()
+	{
+		return name;
+	}
+
+	public void setName (String name)
+	{
+		this.name=name;
+	}
+
+	public int getAge ()
+	{
+		return age;
+	}
+
+	public void setAge (int age)
+	{
+		this.age=age;
+	}
 }
