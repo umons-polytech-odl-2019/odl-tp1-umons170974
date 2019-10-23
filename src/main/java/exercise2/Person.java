738b78199f79961e45e78a05ce018a9717f7be8e
Manuela <170974@umons.ac.be>
2019-10-23 15:37:16 +0200

    :)

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,5 +1,38 @@
 package exercise2;
 
 class Person {
-	int age;
+	private int age;
+	private static int totalAge=0;
+	private static int populationSize=0;
+
+	public Person (int age)
+	{
+		this.age=age;
+		totalAge+=this.age;
+		populationSize++;
+	}
+
+	public static int computePopulationSize()
+	{
+		return populationSize;
+	}
+
+	public static float computeAveragePopulationAge()
+	{
+		if (populationSize!=0)
+		{
+			return (float)totalAge/populationSize;
+		}
+		else
+		{
+			System.out.println("Population vide!");
+			return 0;
+		}
+	}
+
+	public static void resetPopulation()
+	{
+		totalAge=0;
+		populationSize=0;
+	}
 }
